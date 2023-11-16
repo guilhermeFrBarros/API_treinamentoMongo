fe944ea7b011c2e6d9769f080b4f4cfda2592850
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-11-16 18:15:27 -0300

    ejs encludes e varaveis

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,13 +2,11 @@ const HomeModel = require("../models/HomeModel");
 
 //get /
 exports.paginaInicial = (req, res) => {
-  //  req.flash("info", "ola mundo");
-  //  req.flash("secess", "sucesso no carregamento"); 
-  console.log(req.flash("info"), req.flash("secess"));
-  req.session.userari = { teste: "teste2", logado: true, card: false };
-  console.log(req.session.user);
-  res.render('index');
-  return
+  res.render('index', {
+    titulo: 'Este será o título da página',
+    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  });
+  return;
 };
 
 // post /
@@ -17,6 +15,17 @@ exports.trataPost = (req, res, next) => {
   return
 };
 
+
+//get /
+// exports.paginaInicial = (req, res) => {
+//   //  req.flash("info", "ola mundo");
+//   //  req.flash("secess", "sucesso no carregamento"); 
+//   console.log(req.flash("info"), req.flash("secess"));
+//   req.session.userari = { teste: "teste2", logado: true, card: false };
+//   console.log(req.session.user);
+//   res.render('index');
+//   return
+// };
 
 
 // formato objeto
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,6 +1,23 @@
 exports.middleware = ( req, res, next ) => {
     console.log();
+    res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
     console.log("Passei no middleware global");
     console.log();
     next();
 }
+
+  exports.outroMiddleware = (req, res, next) => {
+    next();
+  };
+  
+  exports.checkCsrfError = (err, req, res, next) => {
+    if(err && 'EBADCSRFTOKEN' === err.code) {
+      return res.render('404');
+    }
+  };
+  
+  exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+  };
+  
